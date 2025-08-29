dc810353121d5a79ebae473c9a7c879c8151d9be
birthquake <rashiedtyre@gmail.com>
2025-08-28 22:34:44 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // Component exports for easy importing
 export { default as SymptomTracker } from './SymptomTracker';
 export { default as MedicationTracker } from './MedicationTracker';
+export { default as Timeline } from './Timeline';
